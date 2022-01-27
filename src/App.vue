40015273932631540e83a93b7a65e46b2d0d45a4
wangbaohui <13802885224@139.com>
2022-01-27 09:47:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <learn-element></learn-element>
+  </div>
+</template>
+
+<script>
+import LearnElement from "./components/LearnElement";
+export default {
+  name: 'App',
+  components:{
+    LearnElement
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/LearnElement.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>学习使用element ui</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LearnElement"
+}
+</script>
+
+<style scoped>
+
+</style>
